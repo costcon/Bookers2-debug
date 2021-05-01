a9167707bb4101c908a6f5e069518937021c5a24
costcon <tentekomambo@gmail.com>
2021-05-01 10:19:54 +0000

    Add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
@@ -34,4 +35,6 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+
+
 end
