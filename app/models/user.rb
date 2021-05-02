d0ff54c3a4b96805fd1d16277a0139feb44040a3
costcon <tentekomambo@gmail.com>
2021-05-02 12:23:08 +0000

    Finish comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
-  has_many :book_comments
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   attachment :profile_image, destroy: false
 
